4c50aeebbe86e8deda2e4b7225e644f2de5ddf4e
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-01-07 11:21:38 +0100

    Initial commit

---
+++ MagicPiMirror.Display/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System.Threading;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MagicPiMirror;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SystemOut.MagicPiMirror
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly SpecialDayCalendar specialDayCalendar;
+        private ThreadPoolTimer _clockTimer = null;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            specialDayCalendar = new SpecialDayCalendar();
+            _clockTimer = ThreadPoolTimer.CreatePeriodicTimer(_clockTimer_Tick, TimeSpan.FromMilliseconds(1000));
+        }
+
+        private async void _clockTimer_Tick(ThreadPoolTimer timer)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                ClockLabel.Text = DateTime.Now.ToString("t", new CultureInfo("da-dk"));
+            });
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ClockLabel.Text = DateTime.Now.ToString("t", new CultureInfo("da-dk"));
+            CalendarHeading.Text = GetDanishDayOfWeek();
+
+            var specials = specialDayCalendar.GetSpecials(DateTime.Today);
+            if (specials.Any())
+            {
+                SpecialNote.Text = specials.First().DisplayText;
+            }
+
+            base.OnNavigatedTo(e);
+        }
+
+        private static string GetDanishDayOfWeek()
+        {
+            // TODO: Add localization support
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Friday:
+                    return "Fredag";
+                case DayOfWeek.Monday:
+                    return "Mandag";
+                case DayOfWeek.Saturday:
+                    return "Lørdag";
+                case DayOfWeek.Sunday:
+                    return "Søndag";
+                case DayOfWeek.Thursday:
+                    return "Torsdag";
+                case DayOfWeek.Tuesday:
+                    return "Tirsdag";
+                case DayOfWeek.Wednesday:
+                    return "Onsdag";
+                default:
+                    return "Pludselig var der to mandage på en uge...";
+            }
+        }
+    }
+}
---
+++ MagicPiMirror.Display/SpecialDayCalendar.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MagicPiMirror
+{
+    internal class SpecialDayCalendar
+    {
+        private readonly List<SpecialDay> specialDays = new List<SpecialDay>
+        {
+
+        };
+
+        public IEnumerable<SpecialDay> GetSpecials(DateTime date)
+        {
+            return specialDays.Where(specialDay => specialDay.Date.DayOfYear == DateTime.Now.DayOfYear);
+        }
+    }
+
+    internal class SpecialDay
+    {
+        public string DisplayText
+        {
+            // TODO: Add localization support
+            get
+            {
+                switch (SpecialDayType)
+                {
+                    case SpecialDayType.Birthday:
+                        return $"I dag er det {GetNameWithEnding()} {(DateTime.Now.Year - Date.Year)} års fødselsdag!";
+                    case SpecialDayType.Romantic:
+                        return $"<3 {DateTime.Now.Year - Date.Year} års dag <3";
+                    default: return "Spændende dag i dag!";
+                }
+            }
+        }
+
+        private string GetNameWithEnding()
+        {
+            if (string.IsNullOrEmpty(name))
+                return name;
+            if (name.EndsWith("s"))
+                return name + "'";
+            return name + "s";
+        }
+
+        private readonly string name;
+
+        public DateTime Date { get; set; }
+        public SpecialDayType SpecialDayType { get; set; }
+
+        public SpecialDay(DateTime date, SpecialDayType type, string name)
+        {
+            Date = date;
+            SpecialDayType = type;
+            this.name = name;
+        }
+
+    }
+
+    internal enum SpecialDayType
+    {
+        Birthday,
+        Romantic
+    }
+}
